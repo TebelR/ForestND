503ec4b78f308e41773c8aaad916f9aef71ddd98
x1nni <x1nni@x1nni.xyz>
2024-11-30 01:31:47 -0600

    styling changes

--- client/frontend/src/components/Navbar.js
+++ client/frontend/src/components/Navbar.js
@@ -5,15 +5,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { faCog } from '@fortawesome/free-solid-svg-icons'
-import { faSignOut } from '@fortawesome/free-solid-svg-icons'
+import { faPieChart } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
   return (
-    <div className='Navbar'>
-        <Link to='/Home'><FontAwesomeIcon icon={faHome} /> Home</Link>
-        <Link to='/'><FontAwesomeIcon icon={faSignOut} /> Login</Link>
-        <Link to='/Settings'><FontAwesomeIcon icon={faCog} /> Settings</Link>
-        <Link to='/Profile'><FontAwesomeIcon icon={faUser} /> Profile</Link>
+    <div className='navbar'>
+      <div className='navbarTop'>
+          <Link to='/Home'><FontAwesomeIcon icon={faHome} /><br/>Home</Link>
+          <Link to='/Analytics'><FontAwesomeIcon icon={faPieChart} /><br/>Analytics</Link>
+          <Link to='/REOs'><FontAwesomeIcon icon={faBriefcase} /><br/>REOs</Link>
+      </div>
+      <div classname='navbarBottom'>
+          <Link to='/Settings'><FontAwesomeIcon icon={faCog} /><br/>Settings</Link>
+          <Link to='/Profile'><FontAwesomeIcon icon={faUser} /><br/>Profile</Link>
+      </div>
     </div>
   )
 }
