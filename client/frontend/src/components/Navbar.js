2d6cbec92b90f3e392e5d8b46f9a0432a53ecd25
x1nni <x1nni@x1nni.xyz>
2024-11-30 02:57:07 -0600

    add check in button

--- client/frontend/src/components/Navbar.js
+++ client/frontend/src/components/Navbar.js
@@ -2,11 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "../Styles/Navbar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
-import { faCog } from '@fortawesome/free-solid-svg-icons'
-import { faPieChart } from '@fortawesome/free-solid-svg-icons'
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle, faUser, faHome, faCog, faPieChart, faBriefcase } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
   return (
@@ -15,6 +11,7 @@ function Navbar() {
           <Link to='/Home'><FontAwesomeIcon icon={faHome} /><br/>Home</Link>
           <Link to='/Analytics'><FontAwesomeIcon icon={faPieChart} /><br/>Analytics</Link>
           <Link to='/REOs'><FontAwesomeIcon icon={faBriefcase} /><br/>REOs</Link>
+          <Link to='/Checkin'><FontAwesomeIcon icon={faCheckCircle} /><br/>Check In</Link>
       </div>
       <div classname='navbarBottom'>
           <Link to='/Settings'><FontAwesomeIcon icon={faCog} /></Link>
